93c9534e80284a894eb918ce6782efbac6c9e0c3
Rovak0 <ericjackwong@outlook.com>
2024-08-21 12:19:03 -0400

    Set up the user schema except encrypting

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
 
+//needs a get to get user data based on who is logged in
+    //gets for each of the user data forms
+//needs a post and a put to manage data
+    //post to create new users and new financial data
+    //puts to update finances
+//a delete to clear data
+
 module.exports = router;
