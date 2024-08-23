6c8d0b49223091597befb85b78084f7b2d6e0136
Rovak0 <ericjackwong@outlook.com>
2024-08-23 14:10:58 -0400

    This should work

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const { connect, connection } = require('mongoose');
+// require("dotenv").config();
 
-const connectionString = 'mongodb://127.0.0.1:27017/financeDB';
+// const connectionString = 'mongodb://127.0.0.1:27017/financeDB';
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techmatchup';
 
 connect(connectionString);
 
