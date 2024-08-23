9b0ad1b4664c1664759f9b214a0c1681fafde74d
Rovak0 <ericjackwong@outlook.com>
2024-08-23 11:25:57 -0400

    server side login

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,6 +10,11 @@ const typeDefs = `
         savings: [Float]!
     }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type Query{
         #there is only query yourself
         users: [User]
@@ -32,7 +37,7 @@ const typeDefs = `
             expenses: [Float],
             savings: [Float]
         ): Auth 
-        login(username: String!, password: String!): Auth
+        login(username: String, email: String, password: String!): Auth
 
 
         #adding finances
